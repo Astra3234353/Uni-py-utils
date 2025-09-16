f6d8e3fe5bee41599fd9cc87da16baff66c4b667
AstraDev <Xc0rpi0n145.com@gmail.com>
2025-09-15 19:10:31 -0600

    Sitemas Parte 2

--- sistemas/examPractica.py
+++ sistemas/examPractica.py
@@ -1,18 +1,24 @@
 estEsperados = 3
 
-califTot = [None] * estEsperados
+califTot = []
 lista = [None] * 3
-calif= [None] * 3
+calif= [0] * 3
 
 estNum = 0
 
 for i in range(estEsperados):
   for y in range(3):
-    n = int(input('Ingresa tu calid del examen %d\n' % y))
+    n = int(input('Ingresa tu calid del examen %d\n' % (y + 1)))
     lista[y] = n
     calif[y] += n
 
-  califTot[i] = lista
-
+  califTot += lista
   estNum +=1
+
+print(califTot)
+print(calif)
+print("Calif final de examen 1 fue: %d" % (calif[0] / estEsperados))
+print("Calif final de examen 2 fue: %d" % (calif[1] / estEsperados))
+print("Calif final de examen 3 fue: %d" % (calif[2] / estEsperados))
+
 
---
+++ sistemas/test_01.py
@@ -0,0 +1,8 @@
+data = [None] * 3
+listaCopia = data
+print(f"{listaCopia}\n")
+
+data = [1]
+print(data)
+print(listaCopia)
+
